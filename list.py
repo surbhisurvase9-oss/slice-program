63d3156ad4d5a7baf7777d30bb3a14f7ba674418
surbhi <surbhisurvase9@gmail.com>
2026-05-22 12:15:58 +0530

    Add functionality to print specific characters in names

---
+++ list.py
@@ -0,0 +1,17 @@
+#print Rahul in list
+student_name = ["Sai","Rahul","Rohit","Sita","Gita","Ravi","Kumar"]
+'''for ch in student_name:
+    if ch == "Rahul":
+        print(ch)'''
+'''print(student_name[1:2])'''
+
+#print rahul in reverse
+'''for name in student_name:
+    if name == "Rahul":
+        reverse_name = name[::-1]
+        print(reverse_name)'''
+
+#print h in Rahul
+for name in student_name:
+    if name == "Rahul":
+        print(name[2:3])
